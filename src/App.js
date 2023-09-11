b74065c5b82206f0770b4bd4ec1ea3a393b139a7
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-11 11:03:27 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Singers from './Components/Singers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Content></Content>
+      <Singers></Singers>
+      <Footer></Footer>
     </div>
   );
 }
